9f9ff8745df183e944379132b91f70d645a15c4d
Aliyaan <93915104+AliyaanZBH@users.noreply.github.com>
2024-09-23 16:51:15 +0100

    Test build

--- JuiceBox_GameJam/Assets/Scripts/GameOverScreen.cs
+++ JuiceBox_GameJam/Assets/Scripts/GameOverScreen.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.SocialPlatforms.GameCenter;
 using UnityEngine.UI;
 
 public class GameOverScreen : MonoBehaviour
--- JuiceBox_GameJam/Assets/Scripts/GridGenerator.cs
+++ JuiceBox_GameJam/Assets/Scripts/GridGenerator.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Build.Content;
 using UnityEngine;
 
 public class GridGenerator : MonoBehaviour
